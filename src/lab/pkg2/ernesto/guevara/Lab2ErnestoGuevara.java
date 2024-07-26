cccb099e8dd38097ffa7665ac2e6b75333770ee9
ErnestoGuevaraBonilla <ernestoguevarab28@outlook.com>
2024-07-26 08:11:54 -0600

    Menu Creado

---
+++ src/lab/pkg2/ernesto/guevara/Lab2ErnestoGuevara.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab.pkg2.ernesto.guevara;
+import java.util.Scanner;
+/**
+ *
+ * @author ernes
+ */
+public class Lab2ErnestoGuevara {
+    public static void main(String[] args) {
+        
+        Scanner lea=new Scanner(System.in);
+        
+        while(true){
+            System.out.println("*** Bienvenido al Menu ***");
+            System.out.println("1. Cadena");
+            System.out.println("2. Notas");
+            System.out.println("3. Clases");
+            System.out.println("4. Salir");
+            int op=lea.nextInt();
+            
+            
+                
+            }
+        }
+    }
+    
+
